48baeb8bd10eee5afa57bc9ff4dd984de6ebe4a2
MrMarga <margaghale31@gmail.com>
2023-10-18 17:39:31 +0545

    tetdt

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,14 +1,13 @@
-def call(String dockerHubUsername, String imageName) {
+def call(String dockerHubUsername, String imageName ) {
     // Build the Docker image
     sh "docker build --build-arg REACT_APP_RAPID_API_KEY=b7d0e51456mshf4b33a94aebf47dp139c37jsn74e7d26e47fe -t ${imageName} ."
      // Tag the Docker image
     sh "docker tag ${imageName} ${dockerHubUsername}/${imageName}:latest"
     sh "docker tag ${imageName} ${dockerHubUsername}/${imageName}:${BUILD_NUMBER}"
     // Push the Docker image
-    withDockerRegistry([url: 'https://index.docker.io/v1/', credentialsId: 'docker-cred']) {
-        sh "docker push ${dockerHubUsername}/${imageName}:${BUILD_NUMBER}"
-        sh "docker rmi ${imageName} ${dockerHubUsername}/${imageName}:latest"
-        sh  docker rmi ${imageName} ${dockerHubUsername}/${imageName}:${BUILD_NUMBER"}"
-        
+    withDockerRegistry([url: 'https://index.docker.io/v1/', credentialsId: 'docker']) {
+    sh "docker push ${dockerHubUsername}/${imageName}:latest"    
     }
+    sh "docker rmi ${dockerHubUsername}/${imageName}:latest "
+    sh "docker rmi ${imageName} ${dockerHubUsername}/${imageName}:${BUILD_NUMBER}"
 }
