fc979b148b0264fed31d517507073267d8b232c1
MrMarga <margaghale31@gmail.com>
2023-10-18 18:46:52 +0545

    final testing

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,13 +1,14 @@
 def call(String dockerHubUsername, String imageName ) {
     // Build the Docker image
-    sh "docker build --build-arg REACT_APP_RAPID_API_KEY=b7d0e51456mshf4b33a94aebf47dp139c37jsn74e7d26e47fe -t ${dockerHubUsername}/${imageName} ."
+    sh "docker build --build-arg REACT_APP_RAPID_API_KEY=b7d0e51456mshf4b33a94aebf47dp139c37jsn74e7d26e47fe -t ${imageName} ."
      // Tag the Docker image
-    sh "docker tag ${dockerHubUsername}/${imageName} ${dockerHubUsername}/${imageName}:latest"
-    sh "docker tag ${dockerHubUsername}/${imageName} ${dockerHubUsername}/${imageName}:${BUILD_NUMBER}"
+    sh "docker tag ${imageName} ${dockerHubUsername}/${imageName}:latest"
+    sh "docker tag ${imageName} ${dockerHubUsername}/${imageName}:${BUILD_NUMBER}"
     // Push the Docker image
     withDockerRegistry(credentialsId: 'docker-cred') {
     sh "docker push ${dockerHubUsername}/${imageName}:latest"   
     sh "docker push ${dockerHubUsername}/${imageName}:${BUILD_NUMBER}"
+    sh "docker rmi -f ${imageName}"
     sh "docker rmi -f ${dockerHubUsername}/${imageName} ${dockerHubUsername}/${imageName}:latest"
     sh "docker rmi -f ${dockerHubUsername}/${imageName} ${dockerHubUsername}/${imageName}:${BUILD_NUMBER}"
     }
